7f4bf5836f2a832813e4de207df5e0d5527beba1
Ressie <90820089+ressie-res@users.noreply.github.com>
2026-07-20 22:39:13 +0200

    fix(np): Disallow non integer argument

--- cmd/smh.go
+++ cmd/smh.go
@@ -3,11 +3,19 @@ package cmd
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
 func NewPassword(length string) (result string) {
-	funkystuff := exec.Command("openssl", "rand", "-base64", length)
+
+	num1, err := strconv.Atoi(length)
+	if err != nil {
+		fmt.Print("Error: Input should be a whole number")
+		return
+	}
+
+	funkystuff := exec.Command("openssl", "rand", "-base64", strings.TrimSpace(string(rune(num1))))
 	output, _ := funkystuff.Output()
 	return fmt.Sprintf("%s", strings.TrimSpace(string(output)))
 }
